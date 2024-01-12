e6e3c79f618d1b14519c8507e4ae87246821fc00
elhamsultanyahmad <110190466+elhamsultanyahmad@users.noreply.github.com>
2024-01-12 18:20:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//Trajectory 
+
+/*Figure out the life trajectory of an 
+individuals life based on certain variables that you 
+give through an exam*/
+
+/* 3 Sectors for the final trajectory: 
+- Green - Prosperity 
+- Yellow 
+- Red - 
+
